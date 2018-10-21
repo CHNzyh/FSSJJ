d81da22eafc32caa5bd9acba1dc941169fb028ad
kele <kele@SC-20181021>
2018-10-21 08:56:43 +0800

    新增数据字典管理

---
+++ Application/Admin/Controller/ConfigController.class.php
@@ -0,0 +1,11 @@
+<?php
+namespace Admin\Controller;
+use Think\Controller;
+use Org\Util\Category;
+/*
+ *文件名称：数据字典管理
+ *功能描述：添加、修改、删除数据字典内容
+ */
+Class ConfigController extends CommonController{
+    
+}
