39729e2a1d985474674d3d984d6c3ae0626bf64d
ksboy1999 <373786626@qq.com>
2018-10-26 23:24:05 +0800

    共享文件控制类文件更新

--- Application/Admin/Controller/SharedataController.class.php
+++ Application/Admin/Controller/SharedataController.class.php
@@ -17,11 +17,17 @@ class SharedataController extends CommonController{
     public function add(){
         if(IS_POST){
             $this->checkToken();
+             
             header('Content-Type:application/json; charset=utf-8');
-            echo json_encode(D("Sharedata")->add());
+
+            echo json_encode(D("Sharedata")->addShare());
         }else{
-            $dp = D('Department')->where('id='.session('my_info.department'))->find();
-            $info['filepath']='/sharedata/'.$dp['dename'];
+            
+            $dp = D('Department')->getDepartmentname(session('my_info.department'));
+            
+            $info['url']='/'.C('SHARE_FILEPATH').'/'.$dp['dename'].'/'.date('Y-m-d',  time()).'/';
+            $info['s_stime'] = time();
+            $info['s_etime'] = time();
             $this->assign('info',$info);
             $this->assign('dp',$dp);
             $this->assign('title','添加共享文件');
@@ -31,8 +37,32 @@ class SharedataController extends CommonController{
     /*
      * 编辑共享文件
      */
-    public function edit($id){
+    public function editShare($id){
+         if(IS_POST){
+            $this->checkToken();
+             
+            header('Content-Type:application/json; charset=utf-8');
+
+            echo json_encode(D("Sharedata")->editShare());
+        }else{
+            $info = D('Sharedata')->where('id='.$id)->find();
+            $dp = D('Department')->getDepartmentname(session('my_info.department'));
+
+           // $info['url']='/'.C('SHARE_FILEPATH').'/'.$dp['dename'].'/'.date('Y-m-d',  time()).'/';
+            $this->assign('info',$info);
+            $this->assign('dp',$dp);
+            $this->assign('title','修改共享文件');
+            $this->display('edit');
+        }
+    }
+    public function delShare($id){
+        $result = D('Sharedata')->delShare($id);
         
+        if($result){
+            $this->success("成功删除");
+        }  else {
+            $this->error("删除失败，可能是不存在该ID的记录");
+        }
     }
     /*
      * 查找共享文件
@@ -40,7 +70,7 @@ class SharedataController extends CommonController{
     public function search(){
         $M = M('Sharedata');
         $keys = I('get.');
-        $where = array('id>0');
+        $where = array('a.id>0');
 
         if(!empty($keys)){	        
 
@@ -55,14 +85,16 @@ class SharedataController extends CommonController{
                 $where['s_did'] = session('my_info.department');
 
 
-        $count = $M->where($where)->count();
+        $count = $M->alias('a')->join('__DEPARTMENT__ b ON a.s_did= b.id')->where($where)->count();
         $pConf = page($count,C('PAGE_SIZE')); 
-        $list=$M->where($where)->order('id desc')->limit($pConf['first'], $pConf['list'])->select();
+        $list=$M->alias('a')->join('__DEPARTMENT__ b ON a.s_did= b.id')->join('__ADMIN__ c ON a.s_uid= c.aid')->field('a.*,b.dname,c.nickname')->where($where)->order('a.id desc')->limit($pConf['first'], $pConf['list'])->select();
         $this->list=$list;
 
+        
         $keys['count']=$count;
         $this->keys=$keys;
         $this->page = $pConf['show'];
+
         C('TOKEN_ON',false);
 
         if(session('my_info.aid')==10){//AID=10为超级管理员
@@ -99,6 +131,48 @@ class SharedataController extends CommonController{
      */
     public function showlog(){
         
+    }
+    /*
+     * 删除图片
+     */
+    public function del_pic() {
+        $imgUrl = I('post.imgUrl');
+        $imgDelUrl = C('UPLOAD_PATH').I('post.imgUrl'); 
+        $id = I('post.id');
+        
+        $M = M('Sharedata');
+        $data = array(
+            'id' => $id,
+            's_url' =>''
+        );
+        if($id){
+            if($M->save($data)){
+                if(@unlink($imgDelUrl)){
+                    echo json_encode(array(
+                    'status' => 1,
+                    'msg' => '已从数据库删除成功!'
+                    ));
+                }else{
+                    echo json_encode(array(
+                    'status' => 0,
+                    'msg' => '删除失败，刷新页面重试!'
+                    ));
+                }
+            }
+        }else{
+            if(@unlink($imgDelUrl)){
+                echo json_encode(array(
+                'status' => 1,
+                'msg' => '已从磁盘删除成功!'
+                ));
+            }else{
+                echo json_encode(array(
+                'status' => 0,
+                'msg' => '磁盘文件删除失败，请检查文件是否存在或磁盘权限!'
+                ));
+            }
+            
+        }
     }
     
 }
